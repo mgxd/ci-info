370bf3817b9ac9f930b0be960e439fd14824118d
mathiasg <mathiasg@mit.edu>
2019-09-20 12:53:40 -0400

    rf: package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import versioneer
 
 if __name__ == "__main__":
     setup(
-        name="ci",
+        name="ci-info",
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
     )
