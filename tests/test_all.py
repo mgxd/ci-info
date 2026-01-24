41454cddded7dbbb4df14b1524bdf87fc6fd08f7
mathiasg <mathiasg@stanford.edu>
2026-01-24 01:01:43 -0500

    rf: remove duplicated functionality

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,14 +1,11 @@
 import pytest
 import ci_info
 
-CI_VENDORS = ci_info.vendors
-
-
-def _set_env(vendor, env=None):
+def _set_env(vendor, env=None, pr=False):
     """Create an env dict for the given vendor, optionally simulating a PR."""
     env = env or {}
 
-    val = vendor.get("env")
+    val = vendor.get("pr") if pr else vendor.get("env")
 
     if isinstance(val, str):
         env[val] = "1"
@@ -18,29 +15,10 @@ def _set_env(vendor, env=None):
             env[ev] = "1"
 
     elif isinstance(val, dict):
-        if "env" in val and "includes" in val:
-            env[val["env"]] = val["includes"]
-
-        elif "any" in val:
-            env[val["any"][-1]] = "1"
-        else:
-            for k, v in val.items():
-                env[k] = v
-    return env
-
-
-def _set_pr(vendor, env=None):
-    env = env or {}
-    val = vendor.get("pr")
-    if not val:
-        return env
-
-    if isinstance(val, str):
-        env[val] = "1"
-
-    elif isinstance(val, dict):
         if "env" in val:
-            if "ne" in val:
+            if "includes" in val:
+                env[val["env"]] = val["includes"]
+            elif "ne" in val:
                 env[val["env"]] = f"not{val['ne']}"
             elif "any" in val:
                 env[val["env"]] = val["any"][-1]
@@ -52,10 +30,9 @@ def _set_pr(vendor, env=None):
     return env
 
 
-@pytest.mark.parametrize("vendor", CI_VENDORS)
+@pytest.mark.parametrize("vendor", ci_info.vendors)
 def test_ci_detection(vendor):
-    """Test CI detection and PR detection for each vendor."""
-    # Start with blank environment
+    """Populate blank env and test CI / PR detection for each vendor."""
     env = {}
     assert ci_info.name(env) is None
     assert ci_info.is_ci(env) is False
@@ -70,7 +47,7 @@ def test_ci_detection(vendor):
     # PR not set yet
     if vendor.get("pr"):
         assert ci_info.is_pr(env) is False
-        env = _set_pr(vendor, env)
+        env = _set_env(vendor, env, pr=True)
         info = ci_info.info(env)
         assert info["is_pr"] is True
         assert info["name"] == vendor["name"]
